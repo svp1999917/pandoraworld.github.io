2ad8f703e6e83eca9eeab531cdf817d95cf8a002
svp1999917 <svp1999917@gmail.com>
2023-05-10 23:18:21 +0530

    update

--- gamemark/script.js
+++ gamemark/script.js
@@ -2,7 +2,7 @@ const btns = document.querySelectorAll('.btn');
 
 const links = ['https://apkpure.com/pubg-mobile-for-android/com.tencent.ig/download', 
 'https://translate.google.lk/?sl=en&tl=si&op=translate', 
-'https://example3.com'];
+'https://apkpure.com/pubg-mobile-for-android'];
 
 btns.forEach((btn, index) => {
   btn.addEventListener('click', () => {
