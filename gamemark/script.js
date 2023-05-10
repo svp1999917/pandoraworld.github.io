292db15cd9e799f7677a6cecec8b13bdc4c459c6
svp1999917 <svp1999917@gmail.com>
2023-05-10 23:33:07 +0530

    update

--- gamemark/script.js
+++ gamemark/script.js
@@ -3,8 +3,7 @@ const btns = document.querySelectorAll('.btn');
 const links = ['https://apkpure.com/pubg-mobile-for-android/com.tencent.ig/download', 
 'https://translate.google.lk/?sl=en&tl=si&op=translate', 
 'https://apkpure.com/pubg-mobile-for-android',
-'https://hdtoday.tv/watch-movie/watch-paint-hd-96433.9609040',
-'https://hdtoday.tv/watch-movie/watch-simulant-hd-96358.9603214'];
+'https://hdtoday.tv/watch-movie/watch-paint-hd-96433.9609040'];
 
 btns.forEach((btn, index) => {
   btn.addEventListener('click', () => {
