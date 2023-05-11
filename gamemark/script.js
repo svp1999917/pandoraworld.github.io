b040c05dfec3229fedb04c28ebc34ecfaace2fa8
svp1999917 <svp1999917@gmail.com>
2023-05-11 22:29:52 +0530

    update

--- gamemark/script.js
+++ gamemark/script.js
@@ -1,9 +1,10 @@
 const btns = document.querySelectorAll('.btn');
 
-const links = ['https://apkpure.com/pubg-mobile-for-android/com.tencent.ig/download', 
-'https://translate.google.lk/?sl=en&tl=si&op=translate', 
-'https://apkpure.com/pubg-mobile-for-android',
-'https://hdtoday.tv/watch-movie/watch-paint-hd-96433.9609040'];
+const links = ['https://hdtoday.tv/movie/watch-avatar-hd-19690', 
+'https://hdtoday.tv/movie/watch-avatar-the-way-of-water-hd-79936', 
+'https://hdtoday.tv/movie/watch-iron-man-hd-19680',
+'https://hdtoday.tv/movie/watch-iron-man-2-hd-19665',
+'https://hdtoday.tv/movie/watch-iron-man-3-hd-19562',];
 
 btns.forEach((btn, index) => {
   btn.addEventListener('click', () => {
